be4f8bfcab5020fbe199600d28541fea76f983c2
res <res@localhost.localdomain>
2018-02-02 11:09:06 +0900

    開発メモを最新版に更新

--- doc/memo/_markdowmemo_data/markdown-res.js
+++ doc/memo/_markdowmemo_data/markdown-res.js
@@ -1,27 +1,25 @@
 (function () {
+//markdown-itの設定
+var markdownit = window.markdownit({html:true,xhtmlOut:true,linkify: true,langPrefix:'code-style ',breaks: true});
 
-  // ★markdown-it の設定
-  var markdownit = window.markdownit({
-    html: true,
-    xhtmlOut: true,
-    linkify: true, 
-    langPrefix:   'code-style ',
-    breaks: true
-  });
+//マークダウンを HTML に変換して再セット
+var md = markdownit.render(getHtml("#memo_body"));
+md = moveCodeStyle(md);
+$("body").html(md);
 
-  // ★マークダウンを HTML に変換して再セット
-  var md = markdownit.render(getHtml("#memo_body"));
-  md = md.replace(/<pre><code class="code-style /g, '<pre class="code-style"><code class="');
-  //alert(md);
-  $("body").html(md);
+// 比較演算子が &lt; 等になるので置換
+function getHtml(selector) {
+  var html = $(selector).html();
+  html = html.replace(/&lt;/g, '<');
+  html = html.replace(/&gt;/g, '>');
+  html = html.replace(/&amp;/g, '&');
+  return html;
+}
 
-  // 比較演算子が &lt; 等になるので置換
-  function getHtml(selector) {
-    var html = $(selector).html();
-    html = html.replace(/&lt;/g, '<');
-    html = html.replace(/&gt;/g, '>');
-    html = html.replace(/&amp;/g, '&');
-    return html;
-  }
+// code-styleをpreタグに移動。
+function moveCodeStyle(html) {
+  return html.replace(/<pre><code class="code-style /g
+      , '<pre class="code-style"><code class="');
+}
 
 }());
