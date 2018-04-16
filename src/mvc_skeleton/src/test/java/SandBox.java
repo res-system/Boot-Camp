58b120882e6cde457f0808cd2933512b30aa9320
res <res@localhost.localdomain>
2018-04-16 14:34:50 +0900

    SandBox 追加

---
+++ src/mvc_skeleton/src/test/java/SandBox.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ *
+ * SandBox.
+ *
+ * @author res.
+ *
+ */
+public class SandBox {
+
+    @Test
+    public void SandBoxTest() throws Exception {
+        String testinfo = "[ SandBox ]";
+        System.out.println(testinfo + "** Start ****************");
+
+        System.out.println(testinfo + "**   End ****************");
+    }
+
+}
