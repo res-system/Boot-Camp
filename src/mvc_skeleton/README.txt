1249337d6910f546a498d038efe39db5e936aad7
res <res@localhost.localdomain>
2018-06-28 18:03:24 +0900

    アップロード対応。

--- src/mvc_skeleton/README.txt
+++ src/mvc_skeleton/README.txt
@@ -6,19 +6,22 @@ This software is released under the Apache License, Version2.0, see LICENSE.txt.
 ==============================================================================
 
 
-[License]
+[Library License]
 
 Java Servlet API
-  CDDL 2
+  CDDL / GPL 2.0
 
 Jersey Container Servlet
   CDDL 1.1 / GPL 2.0
 
-Jersey Inject Hk2
-  CDDL 1.1 / GPL 2.0
-  
-Jersey Media Json Jackson
-  CDDL 1.1 / GPL 2.0
+Jersey Inject HK2
+  Apache 2.0 / BSD / CDDL 1.1 / Public
+
+Jersey Media JSON Jackson
+  Apache 2.0 / CDDL 1.1 / GPL 2.0
+
+Jersey Media Multipart
+  Apache 2.0 / BSD / CDDL 1.1 / Public
 
 Thymeleaf
   Apache 2.0
@@ -27,16 +30,19 @@ Weld Servlet Core
   Apache 2.0
 
 Jersey Ext Cdi1x Servlet
-  CDDL 1.1 / GPL 2.0
-
-Mariadb Java Client
-  LGPL 2.1
-
-Apache Log4j API
-  Apache 2.0
-
-Apache Log4j Core
-  Apache 2.0
+  Apache 2.0 / BSD / CDDL 1.1 / Public
 
 JUnit
   EPL 1.0
+
+Apache DeltaSpike Test Control Module API
+  Apache 2.0
+
+Apache DeltaSpike Test Control Module Impl
+  Apache 2.0
+
+Apache DeltaSpike CDI Weld ContainerControl
+  Apache 2.0
+
+Weld SE (Core)
+  Apache 2.0
--- src/mvc_skeleton/src/main/java/com/res_system/mvc_skeleton/controller/TopController.java
+++ src/mvc_skeleton/src/main/java/com/res_system/mvc_skeleton/controller/TopController.java
@@ -1,11 +1,18 @@
 package com.res_system.mvc_skeleton.controller;
 
+import java.io.InputStream;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import com.res_system.commons.mvc.controller.response.HtmlResponse;
 import com.res_system.commons.mvc.model.form.FormUtil;
@@ -41,12 +48,21 @@ public class TopController {
         return new HtmlResponse(TEMPLATE).add("form", form);
     }
 
-    @POST
     @Path("/action")
+    @POST
     public HtmlResponse doAction(MultivaluedMap<String, String> params) throws Exception {
         TopForm form = FormUtil.make(TopForm.class, params);
         model.doAction(form);
         return new HtmlResponse(TEMPLATE).add("form", form);
+    }
+
+    @Path("/upload")
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public String doUpload(
+              @FormDataParam("file") InputStream file
+            , @FormDataParam("file") FormDataContentDisposition dispoosition) throws Exception {
+        return dispoosition.getFileName();
     }
 
 }
